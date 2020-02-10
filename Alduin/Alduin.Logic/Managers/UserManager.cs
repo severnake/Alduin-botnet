c856fb46c531800e7b2537e1336b63e37df16334
Mate <szabo.mate960722@gmail.com>
2020-02-10 19:47:00 +0100

    Created Alduin.Logic.Test and write simple testing methods

--- Alduin/Alduin.Logic/Managers/UserManager.cs
+++ Alduin/Alduin.Logic/Managers/UserManager.cs
@@ -4,6 +4,7 @@ using Alduin.DataAccess.Entities;
 using Alduin.Logic.Interfaces.Managers;
 using Alduin.Shared.DTOs;
 using Alduin.Shared.Interfaces.UnitOfWork;
+using Alduin.Shared.Transaction;
 
 namespace Alduin.Logic.Managers
 {
@@ -11,5 +12,31 @@ namespace Alduin.Logic.Managers
     {
         public UserManager(ISession session, IMapper mapper, IUnitOfWork unitOfWork) : base(session, mapper, unitOfWork)
         { }
+        protected override TransactionResult ValidateSaving(UserEntity entity)
+        {
+            var result = base.ValidateSaving(entity);
+
+            if (string.IsNullOrEmpty(entity.Email))
+            {
+                result.ErrorMessages.Add(new TransactionErrorMessage
+                {
+                    IsPublic = true,
+                    Message = "Email is required!"
+                });
+                result.Succeeded = false;
+            }
+
+            if (string.IsNullOrEmpty(entity.Name))
+            {
+                result.ErrorMessages.Add(new TransactionErrorMessage
+                {
+                    IsPublic = true,
+                    Message = "Name is required!"
+                });
+                result.Succeeded = false;
+            }
+
+            return result;
+        }
     }
 }

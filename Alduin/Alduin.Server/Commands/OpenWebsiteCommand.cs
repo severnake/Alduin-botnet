91b3e9181695c697d313430631a756ba8d99456a
Mogyiii <szabo.mate960722@gmail.com>
2020-05-07 18:18:11 +0200

    added website command

--- Alduin/Alduin.Server/Commands/OpenWebsiteCommand.cs
+++ Alduin/Alduin.Server/Commands/OpenWebsiteCommand.cs
@@ -4,7 +4,14 @@ using System.Text;
 
 namespace Alduin.Server.Commands
 {
-    public class OpenWebsiteCommand : BaseCommands
+
+    public class WebsiteOpenModel
+    {
+        public WebsiteVariables newWebsiteModel { get; set; }
+        public BaseCommands newBaseCommand { get; set; }
+    }
+
+    public class WebsiteVariables
     {
         public string Url { get; set; }
         public bool Hidde { get; set; }

56220012b4a4c94ee5c7fa9fdbd32854f1c0b520
Unknown <unknown@example.com>
2020-05-20 15:33:57 +0200

    Update#2

--- Alduin/Alduin.Stump/Class/Commands/AllImgToJson.vb
+++ Alduin/Alduin.Stump/Class/Commands/AllImgToJson.vb
@@ -5,11 +5,13 @@ Imports Newtonsoft.Json
 Public Class AllImgToJson : Implements ICommand
     <STAThread()>
     Public Shared Function Handler()
-        Dim objReader As New System.IO.StreamReader(Get_LocalPath() & "\Images.txt")
+        Dim reader As New StreamReader(Get_LocalPath() & "\Images.txt", Encoding.Default)
         Dim allImg As New AllImgModel
-        Do While objReader.Peek() <> -1
-            allImg.Imges.Add(objReader.ReadLine())
-        Loop
+        If System.IO.File.Exists(Get_LocalPath() & "\Images.txt") = True Then
+            Do While reader.Peek() <> -1
+                allImg.Imges.Add(reader.ReadLine())
+            Loop
+        End If
         Return JsonConvert.SerializeObject(allImg)
     End Function
 End Class

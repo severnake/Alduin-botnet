0334e09bde7acb9f30c2be854aa07857e4224b2c
Mogyiii <szabo.mate960722@gmail.com>
2020-07-04 14:27:03 +0200

    Source code download

---
+++ Alduin/Alduin.Server/Commands/Commands/GetSourceFileCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alduin.Server.Commands.Commands
+{
+    public class GetSourceFileCommand
+    {
+        public GetFileVariables newVariables { get; set; }
+        public BaseCommands newBaseCommand { get; set; }
+    }
+
+    public class GetFileVariables
+    {
+        public string filePath { get; set; }
+    }
+}
